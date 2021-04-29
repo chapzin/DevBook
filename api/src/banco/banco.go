12b95d1727536791a9cc7caf1d171db7da9990ba
Ricardo Gomes <chapzin@gmail.com>
2021-04-29 10:17:35 -0300

    conexao banco

---
+++ api/src/banco/banco.go
@@ -0,0 +1,21 @@
+package banco
+
+import (
+	"database/sql"
+
+	"github.com/chapzin/DevBook/api/src/config"
+	_ "github.com/lib/pq"
+)
+
+func Conectar() (*sql.DB, error) {
+	db, erro := sql.Open("postgres", config.StringConexaoBanco)
+	if erro != nil {
+		return nil, erro
+	}
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+
+	return db, nil
+}
